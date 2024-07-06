f9d46a2fadec5db35a2f576e801a31cc905d7ef9
meowlet <ngoclink13@gmail.com>
2024-07-06 12:05:39 +0700

    Feature: Group to /api route

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,19 @@ import { ErrorPlugin } from "./plugin/ErrorPlugin";
 import { AuthController } from "./controller/AuthController";
 import { TreeController } from "./controller/TreeController";
 import { NodeController } from "./controller/NodeController";
+import cors from "@elysiajs/cors";
+import { UserController } from "./controller/UserController";
 
 const app = new Elysia()
   .use(ErrorPlugin)
-  .use(AuthController)
-  .use(TreeController)
-  .use(NodeController)
+  .use(cors())
+  .group("/api", (app) =>
+    app
+      .group("/auth", (app) => app.use(AuthController))
+      .use(TreeController)
+      .use(NodeController)
+      .group("/user", (app) => app.use(UserController)),
+  )
   .get("/", () => "Hello Elysia")
   .listen(3000);
 
